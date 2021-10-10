9d512f59f7fcb12ee5c8358d86afb09b6b9cc62b
singhgrtj <singhgrtj@gmail.com>
2021-10-10 15:51:00 +0200

    Create the UI and make it functional

--- src/screens/BulbScreen.tsx
+++ src/screens/BulbScreen.tsx
@@ -1,6 +1,15 @@
 // Component import
 import React, { useState } from 'react';
-import { Text } from 'react-native';
+import {
+    SafeAreaView,
+    Platform,
+    StatusBar,
+    View,
+    StyleSheet,
+    TouchableOpacity,
+    Text
+} from 'react-native';
+import { Icon } from 'react-native-elements';
 
 // Style and constants import
 
@@ -12,9 +21,56 @@ const BulbScreen: React.FC<Props> = (props) => {
     const { } = props;
     const [isOn, setIsOn] = useState<boolean>(false);
 
+    // function to switch on/off the bulb
+    const handleOnOff = () => {
+        setIsOn(!isOn);
+    }
+
     return (
-        <Text>Bulb</Text>
+        <SafeAreaView style={styles.mainContainer}>
+            <StatusBar barStyle="dark-content" hidden={false} backgroundColor={isOn ? "yellow" : "darkgray"} translucent={true}/>
+            <View style={[styles.container, { backgroundColor: isOn ? "yellow" : "darkgray"}]}>
+                {/* Vertical Line */}
+                <View style={styles.verticalLine} />
+
+                {/* Light Bulb */}
+                <TouchableOpacity activeOpacity={1} onPress={handleOnOff}>
+                    <Icon name="bulb-outline" type="ionicon" size={250} style={styles.bulb} color={isOn ? "black" : "gray"}/>
+                </TouchableOpacity>
+
+                <View style={styles.instruction}>
+                    <Text style={styles.instructionText}>Tap on the bulb to turn on/off the light</Text>
+                </View>
+            </View>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    container: {
+        height: '100%',
+        width: '100%',
+        alignItems: 'center' 
+    },
+    verticalLine: {
+        height: 200,
+        width: 6,
+        backgroundColor: 'black'
+    },
+    bulb: {
+        transform: [{ rotateX: '180deg' }]
+    },
+    instruction: {
+        height: 200,
+        justifyContent: 'flex-end'
+    },
+    instructionText: {
+        fontSize: 16,
+    }
+}); 
 
 export default BulbScreen;
